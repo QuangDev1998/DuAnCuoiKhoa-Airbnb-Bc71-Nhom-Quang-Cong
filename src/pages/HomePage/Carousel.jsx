af04509783ae1795c855ffd40263ab69fd0174c6
Quang <leminhquangdev@gmail.com>
2024-12-13 00:55:24 +0700

    update

--- src/pages/HomePage/Carousel.jsx
+++ src/pages/HomePage/Carousel.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import videoCarousel from "../../assets/image/videoCarousel.mp4";
+import videoBanner from "../../assets/image/videoBanner.mp4";
 import swooshHero from "../../assets/image/swoosh-hero.png";
 import airbnbLogo from "../../assets/image/airbnb-1.aabeefedaf30b8c7011a022cdb5a6425.png";
 
@@ -16,7 +16,7 @@ export default function Carousel() {
           playsInline
           className="absolute w-full h-full object-cover"
         >
-          <source src={videoCarousel} type="video/mp4" />
+          <source src={videoBanner} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
 
