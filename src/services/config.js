849d257a546ffb6c5cf24f022bb6c97c4ad12888
congvo-dev <conghtc97@gmail.com>
2025-03-19 15:12:25 +0700

    update token

--- src/services/config.js
+++ src/services/config.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { store } from "../redux/store";
 import { turnOffLoading, turnOnLoading } from "../redux/slices/spinnerSlice";
 const token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA3MSIsIkhldEhhblN0cmluZyI6IjE0LzAzLzIwMjUiLCJIZXRIYW5UaW1lIjoiMTc0MTkxMDQwMDAwMCIsIm5iZiI6MTcxNDA2NDQwMCwiZXhwIjoxNzQyMDU4MDAwfQ.aL6UU86iw9qfiazPYi9hHV3FjYthitqZbK5pBfChSiU";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA3MSIsIkhldEhhblN0cmluZyI6IjA0LzA0LzIwMjUiLCJIZXRIYW5UaW1lIjoiMTc0MzcyNDgwMDAwMCIsIm5iZiI6MTcxNDA2NDQwMCwiZXhwIjoxNzQzODcyNDAwfQ.mCs_Uc9yOttTBIcqgCCUbpVNO-U3VhT9Azo8o6CD--E";
 
 export const http = axios.create({
   baseURL: "https://airbnbnew.cybersoft.edu.vn",
